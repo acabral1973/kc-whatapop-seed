41c43ada0fffa608445d2581d7bb30e0ede08c9b
Alejandro Cabral <cabralejandro@gmail.com>
2017-06-27 13:55:23 +0200

    Blue Path resuelto

--- src/app/publication-date.pipe.ts
+++ src/app/publication-date.pipe.ts
@@ -1,3 +1,4 @@
+import { Pipe, PipeTransform } from '@angular/core';
 import * as moment from 'moment';
 import 'moment/locale/es';
 
@@ -12,3 +13,19 @@ import 'moment/locale/es';
 | tienes que usarla donde proceda. Haciendo                        |
 | 'moment(fecha).fromNow()' obtenemos justo lo que necesitamos.    |
 |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+@Pipe({
+  name: 'PublicationDatePipe'
+})
+
+export class PublicationDatePipe implements PipeTransform {
+
+  transform(fecha: string): string {
+
+    if (fecha) {
+        return 'Publicado hace ' + moment(fecha).fromNow();
+    } else {
+      return 'Fecha de publicación desconocida';
+    }
+  }
+}
